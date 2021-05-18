bd40e5ed53c976bf47da4cc182e0505f342269a8
thanhtuu2110 <67455948+thanhtuu2110@users.noreply.github.com>
2021-05-18 20:14:50 +0700

    Updated PlantList & PlantSetting

--- src/Component/Pages/PlantList.js
+++ src/Component/Pages/PlantList.js
@@ -1,7 +1,8 @@
-import React, {Component,useState} from 'react';
+import React, {useState} from 'react';
 import Navbar from '../Navbars/Navbar'
 import classes from './PlantList.module.css'
 import Plant from '../Plant.js';
+import PlantSetting from './PlantSetting'
 
 
 const PlantList = props => {
@@ -14,28 +15,8 @@ const PlantList = props => {
         return plant.id === currentPlant;
     }
     const [currentPlant, setCurrentPlant] = useState(1);
-    const [waterAmount, setWaterAmount] = useState('');
-    const [minimumMoisture, setMinimumMoisture] = useState('');
-    const [waterMode, setWaterMode] = useState(Plants.find(isCorrect).waterMode);
     const choosePlant = (plantID) => {
-        console.log(plantID);
         setCurrentPlant(plantID);
-    }
-  
-    const waterAmountHandler = (event) =>{
-        setWaterAmount(event.target.value);
-    }
-    const minimumMoistureHandler = (event)=>{
-        setMinimumMoisture(event.target.value);
-    }
-    const waterModeHandler = (event) =>{
-        setWaterMode(event.target.value);
-    }
-    const saveData = () => {
-        Plants.find(isCorrect).waterAmount = waterAmount;
-        Plants.find(isCorrect).minimumMoisture = minimumMoisture;
-        Plants.find(isCorrect).waterMode =waterMode;
-        console.log(Plants)
     }
         return(
         <div>
@@ -47,21 +28,7 @@ const PlantList = props => {
                    )}
                 </div>
                 <div className={classes.columnSetting}>
-                        <h2 className={classes.modal__header} >{Plants.find(isCorrect).name}</h2>
-                        <div className={classes.modal__form}>
-                        <label>Water amount (ml)</label>
-                        <input type="number" name="WaterAmount" onChange={waterAmountHandler}/>
-                        <label>Minimum moisture (%)</label>
-                        <input type="number" name="MinimumMoisture" onChange={minimumMoistureHandler}/>
-                        <label>Water mode</label>
-                        <select name="modes" onChange={waterModeHandler}>
-                            <option value={true}>Auto</option>
-                            <option value={false}>Manual</option>
-                        </select>
-                        <div>Prev Water amount, Minimum moisture, Water mode respectively:</div>
-                        <div>{Plants.find(isCorrect).waterAmount},{Plants.find(isCorrect).minimumMoisture},{Plants.find(isCorrect).waterMode ? 'Auto' : 'Manual'}</div>
-                        <button className={classes.btn} onClick={saveData}>Save</button>
-                    </div>
+                    <PlantSetting plant={Plants.find(isCorrect)}/>
                 </div>
             </div>
         </div>
---
+++ src/Component/Pages/PlantSetting.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+import classes from './PlantSetting.module.css'
+
+
+export default class PlantSetting extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            inputWaterMode:'',
+            inputWaterAmount:'',
+            inputMinimumMoisture:'',
+            inputTimeInterval:'',
+        };
+    }
+    waterAmountHandler = (event) =>{
+        this.setState({inputWaterAmount: event.target.value});
+    }
+    minimumMoistureHandler = (event) => {
+        this.setState({inputMinimumMoisture: event.target.value});
+    }
+    waterModeHandler = (event) => {
+        this.setState({inputWaterMode: event.target.value});
+    }
+    saveHandler = () =>{
+        this.props.plant.waterAmount = this.state.inputWaterAmount;
+        this.props.plant.minimumMoisture = this.state.inputMinimumMoisture;
+        this.props.plant.waterMode = this.state.inputWaterMode;
+        console.log(this.props.plant);
+        this.setState(this.props.plant);
+    }
+    render(){
+        return(<div>
+                <h2 className={classes.modal__header} >{this.props.plant.name}</h2>
+                <div className={classes.modal__form}>
+                <label>Water amount (ml)</label>
+                <input type="number" name="WaterAmount" onChange={this.waterAmountHandler}/>
+                <label>Minimum moisture (%)</label>
+                <input type="number" name="MinimumMoisture" onChange={this.minimumMoistureHandler}/>
+                <label>Water mode</label>
+                <select name="modes" onChange={this.waterModeHandler}>
+                    <option value={true}>Auto</option>
+                    <option value={false}>Manual</option>
+                </select>
+                <div>Current Water amount, Minimum moisture, Water mode respectively:</div>
+                <div>{this.props.plant.waterAmount},{this.props.plant.minimumMoisture},{this.props.plant.waterMode ? "Auto" : "Manual"} 
+                </div>
+                <button className={classes.btn} onClick={this.saveHandler}>Save</button>
+                </div>
+            </div>
+        );
+    }
+}
