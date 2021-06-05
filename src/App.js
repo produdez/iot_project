759c05356519cee62fbe91509d5f4601210115d3
thanhtuu2110 <67455948+thanhtuu2110@users.noreply.github.com>
2021-06-05 21:27:30 +0700

    Revert "Updated"
    This reverts commit a1d7ddbcd74376591d508c1f37ce07a44914ac63.

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import History from "./Component/Pages/HistoryPage"
 import Navbar from './Component/Navbars/Navbar'
 import setupAdaMqttClient from "./API/adafruit";
 import  { useState , useEffect} from "react"
-import SeperateHistory from "./Component/Pages/seperateHistory";
 
 const historyData = [
   {
@@ -59,7 +58,6 @@ export default function App() {
         <Route path="/login"><Login /></Route>
         <Route path="/signup"><Signup /></Route>
         <Route path="/history"><History items={historyData}/></Route>
-        <Route path="/sehistory"><SeperateHistory/></Route>
         <PrivateRoute path ="/plant" component={PlantList}/>
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
         <PrivateRoute path = "/notification" component = {Notification}/>
