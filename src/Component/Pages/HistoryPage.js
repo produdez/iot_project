e2a51516613be13cbd991740802b0065e362c42a
Khoa <khoa.lu123@hcmut.edu.vn>
2021-06-12 12:20:43 +0700

    change palette of history

--- src/Component/Pages/HistoryPage.js
+++ src/Component/Pages/HistoryPage.js
@@ -16,6 +16,9 @@ const DB_ENV_NAMES = [
 ]
 const ABBR_MONTHS = [
   'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'
+] 
+const DISPLAY_TYPES = [
+  'Item', 'Graph'
 ]
 
 const History = (props) => {
@@ -23,6 +26,7 @@ const History = (props) => {
   const [filteredMonth, setFilteredMonth] = useState('All');
   const [filteredSubject, setFilteredSubject] = useState('All');
   const [filteredLatestItems, setFilteredLatestItems] = useState('All');
+  const [displayType, setFilteredDisplayType] = useState(DISPLAY_TYPES[0]);
   const [hasSetListener, setHasSetListener] = useState(false);
   const [firebaseItems, setFirebaseItems] = useState({});
   const ref = firebase.database()
@@ -92,6 +96,10 @@ const History = (props) => {
 
   const latestItemsFilterChangeHandler = (selectedLatestItems) => {
     setFilteredLatestItems(selectedLatestItems);
+  }
+
+  const displayTypeChangeHandler = (selectedDisplayType) => {
+    setFilteredDisplayType(selectedDisplayType);
   }
 
 
