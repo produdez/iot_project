af7755a4c9d56b7b362d81039596bab0e4fd6b8b
Khoa <khoa.lu123@hcmut.edu.vn>
2021-06-13 12:02:18 +0700

    add recent filter in envCond history page. clean code for preparing graph data. clean code for displaying time in history pages

--- src/Component/Pages/HistoryItem.js
+++ src/Component/Pages/HistoryItem.js
@@ -4,12 +4,6 @@ import HistoryDate from './HistoryDate';
 import Card from '../Card';
 import './HistoryItem.css';
 
-function pad(num, size) {
-  num = num.toString();
-  while (num.length < size) num = "0" + num;
-  return num;
-}
-
 const HistoryItem = (props) => {
   return (
     <li>
@@ -17,7 +11,7 @@ const HistoryItem = (props) => {
         <HistoryDate date={props.date} />
         <div className='history-item__description'>
           <h2>{props.description}</h2>
-          <p className='history-item__time'>{`${pad(props.date.getHours(), 2)}:${pad(props.date.getMinutes(), 2)}:${pad(props.date.getSeconds(), 2)}`}</p>
+          <p className='history-item__time'>{props.date.toLocaleTimeString('en-GB')}</p>
         </div>
       </Card>
     </li>
